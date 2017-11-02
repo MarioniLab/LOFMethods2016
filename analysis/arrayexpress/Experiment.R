092316345eca68180caf891f7fbb55e3995d11d1
Aaron Lun <alun@wehi.edu.au>
2017-11-02 11:40:02 +0000

    Updated IDF generation script.

--- analysis/arrayexpress/Experiment.R
+++ analysis/arrayexpress/Experiment.R
@@ -32,14 +32,14 @@ all.out[["Person Address"]] <- "University of Cambridge Li Ka Shing Centre Robin
 all.out[["Person Affiliation"]] <- "Cancer Research UK Cambridge Institute"
 all.out[["Person Roles"]] <- "submitter"
 
-all.out[["Protocol Name"]] <- c("Obtaining HeLa cells",
-                                "Culturing HeLa cells",
-                                "Depleting the lncRNA",
-                                "Extracting RNA",
-                                "Creating libraries",
-                                "Sequencing libraries",
-                                "Assigning reads to genes",
-                                "Reverse transcription")
+all.out[["Protocol Name"]] <- c("P-MTAB-53234", 
+                                "P-MTAB-53235",
+                                "P-MTAB-53236",
+                                "P-MTAB-53237",
+                                "P-MTAB-53238",
+                                "P-MTAB-53239",
+                                "P-MTAB-53240",
+                                "P-MTAB-53241")
 all.out[["Protocol Type"]] <- c("sample collection protocol",
                                 "growth protocol",
                                 "treatment protocol",
@@ -65,13 +65,13 @@ all.out[["Protocol Term Accession Number"]] <- c("EFO_0005518",
                                                  "EFO_0004917",
                                                  "EFO_0005520")
 
-all.out[["Protocol Description"]] <- c("HeLa cells were obtained from American Type Culture Collection. To generate clones expressing dCas9-KRAB, HeLa cells were transduced with lentivirus containing the pHR-SFFV-dCAS9-BFP-KRAB vector along with polybrene (5 ug/ml, Sigma), incubated for 72 hours with medium replacement after 24 hours, and sorted for the BFP-expressing cells using a BD FACSAria III cell sorter.",
+all.out[["Protocol Description"]] <- c("HeLa cells were obtained from American Type Culture Collection. To generate a heterogeneous population of cells expressing dCas9-KRAB, HeLa cells were transduced with a lentiviral vector containing the pHR-SFFV-dCAS9-BFP-KRAB vector along with polybrene (5 ug/ml, Sigma), incubated for 72 hours with medium replacement after 24 hours, and sorted for the BFP-expressing cells using a BD FACSAria III cell sorter. To generate single-cell clones expressing dCas9-KRAB, BFP-sorted single cells were plated on 96-well plates and grown for two weeks until colonies were obtained. Expression of dCas9-KRAB in each colony was verified by Western blot.", 
                                        "HeLa cells were maintained in Dulbecco's modified Eagle's medium (Sigma Aldrich, D6429) supplemented with 10% fetal bovine serum (Thermo Fisher Scientific) and cultured at 37 degrees Celsius with with 5% CO2.",
                                        "For RNAi and LNA, HeLa cells were transfected with Lipofectamine RNAiMax reagent (Thermo Fischer Scientific) following the manufacturer's instructions, using siRNAs (Thermo Fischer Scientifc) and LNA Gapmers (Exiqon) at a final concentration of 50 nM and 25nm, respectively. All experiments were done 48 hr after transfection. For CRISPRi, dCAS9-KRAB transduced cells were plated on a 12-well plate and infected with lentivirus containing the appropriate sgRNA vector. The virus was diluted with HeLa media (1:1 dilution) and cationic polymer polybrene was added to facilitate viral transduction (5 ug/ml, Sigma). After 24 hr incubation, supernatant was removed and fresh media was added for 48 hr before RNA collection. Samples with the same batch number were treated at roughly the same time (within the same month), while the experiment number is nested within batch and refers to cells treated on the same day.",
                                        "RNA (1 ug) was extracted with the RNeasy Kit (QIAGEN) and treated with DNase I following the manufacturer's instructions. RNA quality was assessed using a Total RNA Nano chip with a 2100 Bioanalyzer instrument (Agilent).",
                                        "RNA-seq libraries were prepared from HeLA cells using TruSeq Stranded Total RNA Kit with Ribo-Zero Gold (Illumina, RS-122-2303). Library quality was assessed using a DNA1000 chip with a 2100 Bioanalyzer instrument (Agilent).",
                                        "Indexed libraries were PCR amplified and sequenced on multiple lanes of an Illumina Hiseq 2500 instrument to obtain 125 bp paired-end reads.",
-                                       "Reads were aligned to the hg38 build of the human genome using subread v1.5.0 in paired-end RNA-seq mode with unique mapping. The number of read pairs mapped to the exonic regions of each gene was then counted for each library, using the featureCounts function in Rsubread v1.22.3 with Ensembl GRCh38 version 83. Only alignments with mapping quality scores above 10 and forming reversely stranded fragments were considered during counting.",
+                                       "Reads were aligned to the hg38 build of the human genome using subread v1.5.3 in paired-end RNA-seq mode. The number of read pairs mapped to the exonic regions of each gene was then counted for each library, using the featureCounts function in Rsubread v1.28.0 with Ensembl GRCh38 version 90. Only alignments with mapping quality scores above 10 and forming reversely stranded fragments were considered during counting.",
                                        "The QuantiTect Reverse Transcription Kit (QIAGEN) was used for cDNA synthesis including an additional step to eliminate genomic DNA contamination.")
 all.out[["Protocol Hardware"]] <- c("BD FACSAria III cell sorter",
                                     "", 

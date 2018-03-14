dc690b944d69690797e45ad279b09e1a6ede3b5f
Aaron Lun <alun@wehi.edu.au>
2018-03-14 17:08:20 +0000

    Restored DO numbers in collated metadata file.

--- revisions/data/collate_metadata.R
+++ revisions/data/collate_metadata.R
@@ -4,13 +4,13 @@ tab2 <- read.csv("sample_metadata.csv", header=TRUE, check.names=FALSE)
 combined <- merge(tab1, tab2, by.x="Sample name", by.y="IDs")
 
 # Removing pAs samples.
-metadata <- combined[,c("Barcode", "Var.5")]
-colnames(metadata) <- c("Library", "Group")
+metadata <- combined[,c("Sample name", "Barcode", "Var.5")]
+colnames(metadata) <- c("ID", "Barcode", "Group")
 keep <- !grepl("_pAs$", metadata$Group) 
 metadata <- metadata[keep,]
 
 # Removing a sample that failed.
-metadata <- metadata[metadata$Library!="D705-D507",]
+metadata <- metadata[metadata$Barcode!="D705-D507",]
 
 # Breaking down the groups.
 metadata$Condition <- sub("_exp[0-9]+$", "", metadata$Group)
@@ -21,7 +21,7 @@ metadata$Group <- NULL
 metadata <- metadata[!metadata$Condition %in% c("Malat86", "NOCO", "Monastrol", "hela_cells", "BFP_CAS9"),]
 
 # Replacing dashes with underscores.
-metadata$Library <- sub("-", "_", metadata$Library)
+metadata$Barcode <- sub("-", "_", metadata$Barcode)
 
 ##########################################
 # Setting up the metadata.
@@ -47,7 +47,7 @@ Compound[grepl("^Malat LNA", metadata$Condition)] <- "MALAT1 LNA"
 Compound[grepl("^LNA A", metadata$Condition)] <- "Negative control A"
 
 write.table(file="metadata.tsv", 
-            data.frame(Library=metadata$Library,
+            data.frame(Library=tolower(metadata$ID),
                        Condition=gsub("[ -]", "_", paste(LOF, Genotype, Compound, paste0("batch_", Batch), sep=".")),
                        Experiment=metadata$Experiment,
                        Date=Date, Batch=Batch, LOF=LOF, Genotype=Genotype, Compound=Compound),                                   
@@ -55,8 +55,9 @@ write.table(file="metadata.tsv",
 
 ##########################################
 # Identifying columns to remove in the count table.
+
 counts <- read.table("genic_counts.tsv", header=TRUE, check.names=FALSE)
 re.names <- sub("SLX-[0-9]+\\.", "", colnames(counts))
 re.names <- sub("\\..*", "", re.names)
-counts <- counts[, re.names %in% c("GeneID", "Length", metadata$Library)]
+counts <- counts[, re.names %in% c("GeneID", "Length", metadata$Barcode)]
 write.table(file="subcounts.tsv", counts, sep="\t", quote=FALSE, row.names=FALSE)

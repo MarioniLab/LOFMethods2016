589a3f12acd7869090768dd392ee2f835958fc35
Aaron Lun <alun@wehi.edu.au>
2018-03-17 12:24:28 +0000

    Switched to local paths for mapping/counting scripts.

--- data/count_me.sh
+++ data/count_me.sh
@@ -3,6 +3,6 @@ anno.files <- file.path("../genomes/annotation", c("hg38.gtf", "cas9_pHR_approx.
 bam.files <- list.files("bam", full=TRUE, pattern="bam$")
 ispet <- TRUE
 strandspec <- 2
-' | cat - ~/Code/mapping/counter.R > count_me.R
+' | cat - ../tools/counter.R > count_me.R
 R CMD BATCH --no-save count_me.R 
 
--- revisions/data/mapme.sh
+++ revisions/data/mapme.sh
@@ -2,4 +2,4 @@ ispet=1
 fastq=($(ls fastq/*.fq.gz))
 genome=../../genomes/builds/hg38_cas9
 
-source ${HOME}/Code/mapping/multi_align.sh
+source ../../tools/multi_align.sh

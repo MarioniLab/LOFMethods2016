8aa6bf35834fb58ab4e39a25b1a9002a40b06cfa
Aaron Lun <alun@wehi.edu.au>
2018-03-17 12:21:07 +0000

    Switched to local paths for annotation and genome builds.

--- data/mapme.sh
+++ data/mapme.sh
@@ -1,7 +1,5 @@
-# Requires the mapping file in https://github.com/LTLA/CRUKtools.
-# Requires the genome builds in https://github.com/MarioniLab/CommonResources.
 ispet=1
 fastq=($(ls fastq/*.fq.gz))
-genome=/lustre/jmlab/resources/genomes/subread/hg38_cas9
+genome=../genomes/builds/hg38_cas9
 
 source ${HOME}/Code/mapping/multi_align.sh
--- revisions/data/count_me.sh
+++ revisions/data/count_me.sh
@@ -1,6 +1,5 @@
-# Requires the counting scripts in https://github.com/LTLA/CRUKtools.
 echo '
-anno.files <- file.path("/lustre/jmlab/resources/annotation", c("processed/hg38.gtf", "original/cas9_pHR_approx.gtf"))
+anno.files <- file.path("../../genomes/annotation", c("hg38.gtf", "cas9_pHR_approx.gtf"))
 bam.files <- list.files("bam", full=TRUE, pattern="bam$")
 ispet <- TRUE
 strandspec <- 2

1c24e379201ab75d0402953ca7741424b3456539
Aaron Lun <alun@wehi.edu.au>
2018-03-22 19:30:27 +0000

    Updated the download script.

--- download.sh
+++ download.sh
@@ -4,3 +4,11 @@ wget https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-5308/E-MTAB-5308.processed.
 unzip E-MTAB-5308.processed.1.zip
 rm E-MTAB-5308.processed.1.zip
 
+cd ../revisions/data
+ln -s ../../data/E-MTAB-5308.sdrf.txt
+
+# Also cloning the tools (using the last commit known to work).
+cd ../
+git clone https://github.com/LTLA/CRUKTools tools
+cd tools
+git reset --hard 915706ac016f6f59e74b4ec266f06349293b997f
